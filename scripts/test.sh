d5ee1ab9a52c80c9910b58ed593737e86d3bd013
Omris-Elda <ed.prickett@hotmail.com>
2020-08-27 16:23:42 +0100

    Added test script

---
+++ scripts/test.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+pwd
+ls
+cd app1
+pytest --cov app --cov-report term-missing
+cd ..
+cd app2
+pytest --cov app --cov-report term-missing
+cd ..
+cd app3
+pytest --cov app --cov-report term-missing
+cd ..
+cd app4
+pytest --cov app --cov-report term-missing
+cd ..
