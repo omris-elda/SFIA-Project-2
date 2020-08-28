342988a80cd7f73ba049e65c496928996359c6ac
Omris-Elda <ed.prickett@hotmail.com>
2020-08-28 10:53:42 +0100

    Working on Docker Build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,14 @@
 pwd
 ls
 ssh swarm-manager
+ls
 pwd
 whoami
 git clone https://github.com/omris-elda/SFIA-Project-2.git
-# cd SFIA-Project-2
-sudo docker stack deploy --compose-file docker-compose.yaml SFIA-Project-2
+ls
+cd SFIA-Project-2
+sudo docker stack deploy --compose-file docker-compose.yaml project2
 sudo docker images
 sudo docker container ls
 rm -r SFIA-Project-2
+ls
