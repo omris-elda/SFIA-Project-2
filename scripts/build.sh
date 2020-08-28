e00af2119d9e132c55eb425dcdc73ebaf1a0382d
Omris-Elda <ed.prickett@hotmail.com>
2020-08-28 09:56:32 +0100

    Working on Docker Build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,3 +8,14 @@ ls
 docker --version
 docker-compose --version
 sudo docker-compose build
+sudo docker images
+# First we have to tag the images with the username of the registry we want to push them to
+sudo docker tag app1 omriselda/app1:latest
+sudo docker tag app2 omriselda/app2:latest
+sudo docker tag app3 omriselda/app3:latest
+sudo docker tag app4 omriselda/app4:latest
+# Then we can actually push them to the registry (Dockerhub)
+sudo docker push omriselda/app1:latest
+sudo docker push omriselda/app2:latest
+sudo docker push omriselda/app3:latest
+sudo docker push omriselda/app4:latest
